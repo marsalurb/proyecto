adc11810b7c382ae47c3edc0fa83aa052ce95fe4
User <maria.su.1997@hotmail.com>
2018-03-07 18:45:01 +0100

    ola

---
+++ database/migrations/2018_03_06_111845_create_item_suppliers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_suppliers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->unsignedInteger('item_id');
+            $table->unsignedInteger('supplier_id');
+
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_suppliers');
+    }
+}
